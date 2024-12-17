df018859100c962fa16c5d6ba95cb678bfa3b2b4
SHAKTHI <shakthi.amarnath@gmail.com>
2024-12-17 11:56:45 +0530

    update workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
 # Sync local files to the remote server
-rsync -avz --exclude 'node_modules' /mnt/d/github/Client_relationship_management_portal/ crmadmin@139.59.73.56:posspole
+rsync -avz --no-g --chmod=ugo=rwX --exclude 'node_modules' /mnt/d/github/Client_relationship_management_portal/ crmadmin@139.59.73.56:posspole
 
 # SSH into the remote server and restart the Docker container
 ssh crmadmin@139.59.73.56 << 'ENDSSH'
-  cd posspole  # Ensure you are in the correct directory
+  cd posspole
 
   # Stop and remove old containers
   docker-compose down
 
-  # Build and run the Docker container with the latest updates, and keep it running
-  docker-compose up -d --build
+  # Remove unused Docker images
+  docker image prune -af
 
-  # Check container logs and health status
+  # Build Docker images without cache
+  docker-compose build --no-cache
+
+  # Start containers
+  docker-compose up -d
+
+  # Verify containers are running
   docker ps
 ENDSSH
